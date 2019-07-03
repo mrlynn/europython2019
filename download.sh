fe31c34e7090d8d39870707a7f1ffabbfe63f4cf
mrlynn <merlynn@gmail.com>
2019-07-03 15:47:01 -0400

    add source files

--- download.sh
+++ download.sh
@@ -2,6 +2,8 @@
 PIP=/usr/local/bin/pip
 WGET=/usr/local/bin/wget
 DOWNLOADDIR=./usbdrive/requirements/
+SOURCEFILES=./files
+TARGETFILES=./usbdrive
 COMPASSDIR=./usbdrive/mongodb/compass
 MONGODBDIR=./usbdrive/mongodb/server
 REQUIREMENTSFILE=./usbdrive/requirements.txt
@@ -14,5 +16,6 @@ $WGET https://downloads.mongodb.com/compass/mongodb-compass-community-1.18.0-win
 $WGET https://downloads.mongodb.com/compass/mongodb-compass-community-1.18.0-win32-x64.msi
 $WGET https://fastdl.mongodb.org/src/mongodb-src-r4.0.10.tar.gz
 $WGET https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-4.0.10-signed.msi
+cp $SOURCEFILES/* $TARGETFILES
 mv mongodb-compass* $COMPASSDIR
 mv mongodb* $MONGODBDIR
